8f588ad0239b4e660c560f26754a5a6234f61b4b
danielefongo <danielefongo@gmail.com>
2020-02-14 14:47:22 +0100

    add test to check custom metric

--- src/test/kotlin/daikon/prometheus/PrometheusMetricsTest.kt
+++ src/test/kotlin/daikon/prometheus/PrometheusMetricsTest.kt
@@ -8,11 +8,25 @@ import org.junit.jupiter.api.Test
 class PrometheusMetricsTest {
 
     @Test
-    fun `can publish meters`() {
+    fun `can publish metrics`() {
         HttpServer(5555)
             .prometheus("/foo")
             .start().use {
                 assertThat(get("http://localhost:5555/foo").text).contains("jvm")
+            }
+    }
+
+    @Test
+    fun `custom metrics`() {
+        HttpServer(5555)
+            .prometheus("/foo")
+            .get("/bar") { _, _, ctx ->
+                ctx.meterRegistry().counter("calls").increment()
+            }
+            .start().use {
+                get("http://localhost:5555/bar")
+                get("http://localhost:5555/bar")
+                assertThat(get("http://localhost:5555/foo").text).contains("calls_total 2.0")
             }
     }
 }
